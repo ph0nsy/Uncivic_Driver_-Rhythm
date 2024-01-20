41fb993df4ada9fd3132e3e759b15535273ed339
Viskocrack <mariano.delucas.travesi@alumnos.upm.es>
2024-01-20 18:28:01 +0100

    add beat timer script

---
+++ Uncivic_Driver_Rhythm/Assets/Scripts/BeatCheck.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatCheck : MonoBehaviour
+{
+
+    // Active frame constants
+    int CHECK_FRAMES = 15;
+    int GOOD_FRAMES = 10;
+    int EXCELLENT_FRAMES = 4;
+    int CHECK_BAD_FRAMES;
+    int CHECK_GOOD_FRAMES;
+
+
+    int framecount;
+    bool checking = false;
+    bool isFlip = false;
+    int points;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {  
+        this.CHECK_BAD_FRAMES = CHECK_FRAMES-GOOD_FRAMES;
+        this.CHECK_GOOD_FRAMES = (GOOD_FRAMES-EXCELLENT_FRAMES)/2;
+        this.framecount=0;
+        this.checking=false;
+        this.points=0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        //0 es nada, 1 es dcha y -1 izq
+        float input = Input.GetAxis("Horizontal");
+
+        if(this.checking){
+
+            if(input!=0 && isCorrectAction(input)){
+                this.points = checkFrame(framecount);
+            }
+            if(framecount >= CHECK_FRAMES){
+                this.checking=false;
+                this.points=-1;
+            }
+            ++framecount;
+        }
+    }
+
+    
+    bool isCorrectAction(float input){
+        return (input < 0 && isFlip) || (input > 0 && !isFlip) || input == 0;
+    }
+
+    int checkFrame(int frameWithInput){
+
+        // Good!! frame llega a dar puntos
+        if(frameWithInput >= CHECK_BAD_FRAMES
+            && frameWithInput < CHECK_FRAMES){
+            
+            // EXCELLENT BEAT! frame en window de 4 frames
+            if (frameWithInput  > CHECK_BAD_FRAMES + CHECK_GOOD_FRAMES
+                && frameWithInput <= CHECK_BAD_FRAMES + CHECK_GOOD_FRAMES + EXCELLENT_FRAMES){
+                    return 100;
+            }
+
+            // else...
+            return 10;
+        }
+
+        //si lo haces muy pronto o te lo saltas, pierdes un punto
+        return -1;
+
+    }
+}
---
+++ Uncivic_Driver_Rhythm/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+
+    Vector3 mov;
+    float t = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world");   
+        mov = this.GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void FixedUpdate()
+    {   
+        //X^2+y^2=R
+        int R = 5;
+        t+=1*Time.deltaTime;
+        mov = Vector3.left *2* R*Mathf.Cos(3*t) +  Vector3.up * R*Mathf.Sin(t) + Vector3.forward * R*Mathf.Sin(t);
+
+        this.transform.position=mov;
+    }
+}
