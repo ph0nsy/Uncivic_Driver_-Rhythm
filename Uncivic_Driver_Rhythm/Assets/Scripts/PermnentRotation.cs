80b47edd4daaa1ba98b78e571a7e9d7af848b77a
ph0nsy <7836004@alumnos.ufv.es>
2024-01-21 02:28:50 +0100

    spawner

---
+++ Uncivic_Driver_Rhythm/Assets/Scripts/PermnentRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PermnentRotation : MonoBehaviour
+{
+
+    public float tiltx = 15.0f; // rotación a realizar 
+    // 
+    void Update()
+    {
+        transform.Rotate(0, tiltx * Time.deltaTime, 0);
+    }
+}
---
+++ Uncivic_Driver_Rhythm/Assets/Scripts/Spawner.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject car;
+    public GameObject cop;
+    public TextAsset textFile;
+    string textContent;
+    List<Tuple<string, int>> carsPerSong;
+    int fpsCounter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textContent = textFile.text;  //this is the content as string
+        carsPerSong = new List<Tuple<string, int>>();
+        textToMap(textContent);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if(carsPerSong.Count > 0) return;
+        if(fpsCounter == 0){
+            if(carsPerSong[0].Item1 == "cop") Instantiate(car, new Vector3(5f,46.2f,-23.6f), new Quaternion(-35f,0f,0f,1f));
+            else Instantiate(cop, new Vector3(5f,46.2f,-23.6f), new Quaternion(-35f,0f,0f,1f));
+        } else if (fpsCounter == carsPerSong[0].Item2){
+            carsPerSong.RemoveAt(0);
+            fpsCounter = 0;
+        } else fpsCounter++;
+    }
+
+    void textToMap(string text){
+        char[] separators = { ';', '\n' };
+        string[] songSeed = text.Split(separators);
+        int iter = 0;
+        string current = songSeed[iter];
+        while(current != "-"){
+            carsPerSong.Add(new Tuple<string, int>(current, Int32.Parse(songSeed[iter+1])));
+            iter+=2;
+            current = songSeed[iter];
+        }
+    }
+}
